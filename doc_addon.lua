c77173933d838f32b8b4987292d9369a3e114ab6
Alexander Weber <web.alexander@web.de>
2017-03-17 17:56:59 +0100

    removed group handling from doc_addon since doc_addon have own doors workarounds not compatible to the smartfs cache workarounds

--- doc_addon.lua
+++ doc_addon.lua
@@ -3,35 +3,25 @@ local doc_addon = {}
 
 function doc_addon.is_revealed_item(itemname, playername)
 	local cache = smart_inventory.cache
-	if minetest.registered_items[itemname] == nil then
+	local itemdef = minetest.registered_items[itemname]
+	if not itemdef then
 		return false
 	end
 
 	if smart_inventory.doc_items_mod then
 		local category_id
-		if not cache.citems[itemname] then
-			-- not in creative or something like
-			return false
+		if itemdef.type == "node" then
+			category_id = "nodes"
+		elseif itemdef.type == "tool" then
+			category_id = "tools"
+		elseif itemdef.type == "craft" then
+			category_id = "craftitems"
+		end
+		if category_id then
+			return doc.entry_revealed(playername, category_id, itemname)
 		else
-			for _, group in pairs(cache.citems[itemname].cgroups) do
-				if group.name == "type:node" then
-					if cache.citems[itemname].cgroups["group:door"] then
-						category_id = "craftitems" -- map back for doc compatibility
-					else
-						category_id = "nodes"
-					end
-				elseif group.name == "type:tool" then
-					category_id = "tools"
-				elseif group.name == "type:craft" then
-					category_id = "craftitems"
-				end
-			end
-			if category_id then
-				return doc.entry_revealed(playername, category_id, itemname)
-			else
-				-- unknown item
-				return false
-			end
+			-- unknown item
+			return false
 		end
 	end
 	return true
