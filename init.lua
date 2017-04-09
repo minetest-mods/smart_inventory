b63c6e4512a4a3a60ff4c3474e5792fe0d5cf816
Alexander Weber <web.alexander@web.de>
2017-04-09 18:25:10 +0200

    moved libs to "libs" folder

--- init.lua
+++ init.lua
@@ -5,8 +5,8 @@ smart_inventory.skins_mod = minetest.get_modpath("skins")
 smart_inventory.armor_mod = minetest.get_modpath("3d_armor")
 smart_inventory.doc_items_mod = minetest.get_modpath("doc_items")
 smart_inventory.registered_pages = {}
-smart_inventory.smartfs = dofile(modpath.."/smartfs.lua")
-smart_inventory.smartfs_elements = dofile(modpath.."/smartfs-elements.lua")
+smart_inventory.smartfs = dofile(modpath.."/libs/smartfs.lua")
+smart_inventory.smartfs_elements = dofile(modpath.."/libs/smartfs-elements.lua")
 smartfs = smart_inventory.smartfs
 
 
@@ -128,9 +128,9 @@ else
 	smart_inventory.txt = dofile(modpath.."/classify_description_en.lua")
 end
 
-smart_inventory.filter = dofile(modpath.."/filter.lua")
+smart_inventory.filter = dofile(modpath.."/libs/filter.lua")
 smart_inventory.doc_addon = dofile(modpath.."/doc_addon.lua")
-smart_inventory.cache = dofile(modpath.."/cache.lua")
+smart_inventory.cache = dofile(modpath.."/libs/cache.lua")
 smart_inventory.ui_tools = dofile(modpath.."/ui_tools.lua")
 
 -- register pages
