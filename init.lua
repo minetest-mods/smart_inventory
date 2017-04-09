6d4eb9e7d0cfc184eda02184b15c73c0e6dd05ee
Alexander Weber <web.alexander@web.de>
2017-04-09 18:09:17 +0200

    move pages lua files to "pages" folder

--- init.lua
+++ init.lua
@@ -134,13 +134,13 @@ smart_inventory.cache = dofile(modpath.."/cache.lua")
 smart_inventory.ui_tools = dofile(modpath.."/ui_tools.lua")
 
 -- register pages
-dofile(modpath.."/crafting.lua")
+dofile(modpath.."/pages/crafting.lua")
 
 if minetest.setting_getbool("creative_mode") then
-	dofile(modpath.."/creative.lua")
+	dofile(modpath.."/pages/creative.lua")
 end
 
 if smart_inventory.skins_mod or smart_inventory.armor_mod then
-	dofile(modpath.."/player.lua")
+	dofile(modpath.."/pages/player.lua")
 end
 
