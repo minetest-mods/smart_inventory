9ede35bb0a5079e0f812bf2d41c06854fcddd435
Alexander Weber <web.alexander@web.de>
2017-04-09 18:29:52 +0200

    moved framework to inventory_framework.lua

---
+++ inventory_framework.lua
@@ -0,0 +1,105 @@
+smartfs = smart_inventory.smartfs
+
+-- start with empty group items replacement table.
+-- Will be filled at runtime with used items. partially independend on user
+
+local inventory_form = smartfs.create("smart_inventory:main", function(state)
+	-- tabbed view controller
+	local tab_controller = {
+		_tabs = {},
+		active_name = nil,
+		set_active = function(self, tabname)
+			for name, def in pairs(self._tabs) do
+				if name == tabname then
+					def.button:setBackground("halo.png")
+					def.view:setVisible(true)
+				else
+					def.button:setBackground(nil)
+					def.view:setVisible(false)
+				end
+			end
+			self.active_name = tabname
+		end,
+		tab_add = function(self, name, def)
+			def.viewstate:size(20,10) --size of tab view
+			self._tabs[name] = def
+		end,
+		get_active_name = function(self)
+			return self.active_name
+		end,
+	}
+
+	--set screen size
+	state:size(20,12)
+	state:label(1,0.2,"header","Smart Inventory")
+	state:image(0,0,1,1,"header_logo", "logo.png")
+--	state:image_button(19,0,1,1,"exit", "Exit","???.png", true)
+	state:button(19,0,1,1,"exit", "Exit", true)
+	local button_x = 0.1
+	table.sort(smart_inventory.registered_pages, function(a,b)
+		if not a.sequence then
+			return false
+		elseif not b.sequence then
+			return true
+		elseif a.sequence > b.sequence then
+			return false
+		else
+			return true
+		end
+	end)
+	for _, def in ipairs(smart_inventory.registered_pages) do
+		assert(def.smartfs_callback, "Callback function needed")
+		assert(def.name, "Name is needed")
+		local tabdef = {}
+		local label
+		if not def.label then
+			label = ""
+		else
+			label = def.label
+		end
+		tabdef.button = state:button(button_x,11.2,1,1,def.name.."_button",label)
+		if def.icon then
+			tabdef.button:setImage(def.icon)
+		end
+		tabdef.button:setTooltip(def.tooltip)
+		tabdef.button:onClick(function(self)
+			tab_controller:set_active(def.name)
+			if def.on_button_click then
+				def.on_button_click(tabdef.viewstate)
+			end
+		end)
+		tabdef.view = state:container(0,1,def.name.."_container")
+		tabdef.viewstate = tabdef.view:getContainerState()
+		def.smartfs_callback(tabdef.viewstate)
+		tab_controller:tab_add(def.name, tabdef)
+		button_x = button_x + 1
+	end
+	tab_controller:set_active(smart_inventory.registered_pages[1].name)
+end)
+
+smartfs.set_player_inventory(inventory_form)
+
+
+function smart_inventory.register_page(def)
+	table.insert(smart_inventory.registered_pages, def)
+end
+
+function smart_inventory.get_page_state(pagename, playername)
+	local rootstate = smart_inventory.smartfs.inv[playername]
+	if not rootstate then
+		return
+	end
+	local view = rootstate:get(pagename.."_container")
+	if not view then
+		return
+	end
+	return view:getContainerState()
+end
+
+function smart_inventory.get_registered_page(pagename)
+	for _, registred_page in ipairs(smart_inventory.registered_pages) do
+		if registred_page.name == pagename then
+			return registred_page
+		end
+	end
+end
